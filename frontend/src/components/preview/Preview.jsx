5aa1a1efc3b952b5d89f42417aa24a3bf26404cc
roconnorr <roryoconnor@outlook.co.nz>
2019-06-16 12:30:20 +1200

    Return of the mark

--- frontend/src/components/preview/Preview.jsx
+++ frontend/src/components/preview/Preview.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactMarkdown from 'react-markdown';
 
 import './Preview.css';
 
-const Preview = ({ content }) => <div>{content}</div>;
+const Preview = ({ content }) => <ReactMarkdown source={content} />;
 
 Preview.propTypes = {
   content: PropTypes.string.isRequired,
