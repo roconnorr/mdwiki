e3b81a0cfba6d841c664257083764d3e98e55fa6
roconnorr <roryoconnor@outlook.co.nz>
2019-06-25 22:06:48 +1200

    Added listItem renderer from the react-markdown library so event listeners can be attached to checkboxes

--- frontend/src/components/preview/Preview.jsx
+++ frontend/src/components/preview/Preview.jsx
@@ -4,11 +4,26 @@ import ReactMarkdown from 'react-markdown';
 
 import './Preview.css';
 
+const { createElement } = React;
+
+// copied from react-markdown for passing props to element renderers
+function getCoreProps(props) {
+  return props['data-sourcepos'] ? { 'data-sourcepos': props['data-sourcepos'] } : {};
+}
+
 const Preview = ({ content }) => (
   <ReactMarkdown
     source={content}
     renderers={{
-      link: props => <a href={props.href} target="_blank" rel="noopener noreferrer">{props.children}</a>
+      link: props => <a href={props.href} target="_blank" rel="noopener noreferrer">{props.children}</a>,
+      listItem: (props) => {
+        const { checked, children } = props;
+        let checkbox = null;
+        if (checked !== null) {
+          checkbox = createElement('input', { type: 'checkbox', checked, readOnly: true });
+        }
+        return createElement('li', getCoreProps(props), checkbox, children);
+      },
     }}
   />
 );
