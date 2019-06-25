5a02ffe9775a4150a0c7484d26d04964257d381d
roconnorr <roryoconnor@outlook.co.nz>
2019-06-25 21:34:30 +1200

    Added simple link rendering component

--- frontend/src/components/preview/Preview.jsx
+++ frontend/src/components/preview/Preview.jsx
@@ -4,7 +4,14 @@ import ReactMarkdown from 'react-markdown';
 
 import './Preview.css';
 
-const Preview = ({ content }) => <ReactMarkdown source={content} />;
+const Preview = ({ content }) => (
+  <ReactMarkdown
+    source={content}
+    renderers={{
+      link: props => <a href={props.href} target="_blank" rel="noopener noreferrer">{props.children}</a>
+    }}
+  />
+);
 
 Preview.propTypes = {
   content: PropTypes.string.isRequired,
