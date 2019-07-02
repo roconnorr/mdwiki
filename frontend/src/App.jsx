648e339508ac81afe94db0f38e32946686a40009
roconnorr <roryoconnor@outlook.co.nz>
2019-07-03 11:49:18 +1200

    Made the frontend POST the editor contents to the backend on save, with spinner feedback and toast for success/failure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Alignment, Button, Navbar } from '@blueprintjs/core';
+import {
+  Alignment, Button, Navbar, Spinner, Toaster, Intent,
+} from '@blueprintjs/core';
 
 import './App.css';
 
@@ -10,15 +12,54 @@ class App extends Component {
     super(props);
     this.state = {
       pages: [],
+      isSaving: false,
     };
 
     this.editorRef = React.createRef();
   }
 
   fetchPages = () => {
-    fetch('http://localhost:8080/api/pages')
+    fetch('http://localhost:8080/api/page')
       .then(res => res.json())
       .then(pages => this.setState({ pages }));
+  };
+
+  savePage = async () => {
+    // temp refhax for testing
+    const editorContent = this.editorRef.current.editor.innerText;
+
+    this.setState({ isSaving: true });
+    const response = await fetch('http://localhost:8080/api/page', {
+      method: 'POST',
+      body: JSON.stringify({ content: editorContent }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.status === 201) {
+      setTimeout(() => {
+        this.setState({ isSaving: false });
+        Toaster.create().show({
+          icon: 'tick',
+          intent: Intent.SUCCESS,
+          message: 'Saved Successfully!',
+          timeout: 3000,
+        });
+      }, 500);
+    } else {
+      const data = await response.json();
+      console.log(data);
+      setTimeout(() => {
+        this.setState({ isSaving: false });
+        Toaster.create().show({
+          icon: 'warning-sign',
+          intent: Intent.DANGER,
+          message: 'Saving was not successful.',
+          timeout: 3000,
+        });
+      }, 500);
+    }
   };
 
   onEditorClick = () => {
@@ -26,6 +67,7 @@ class App extends Component {
   };
 
   render() {
+    const { isSaving } = this.state;
     return (
       <div className="App">
         <Navbar>
@@ -33,7 +75,12 @@ class App extends Component {
             <Navbar.Heading>MdWiki</Navbar.Heading>
             <Navbar.Divider />
             <Button className="bp3-minimal" icon="home" text="Home" />
-            <Button className="bp3-minimal" icon="document" text="Files" />
+            <Button
+              className="bp3-minimal"
+              icon={isSaving ? <Spinner size={16} /> : 'document'}
+              text="Save"
+              onClick={this.savePage}
+            />
           </Navbar.Group>
         </Navbar>
 
